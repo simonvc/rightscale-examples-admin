1d3921488465f88ecee5f596ad00d4e444db5378
Jonathan Dade <jonathandade@gmail.com>
2011-11-22 19:31:33 +0000

    Added some stuff

---
+++ curl-run-script-with-params.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -ex
+#
+# This is a template for use testing API functions.
+# It relies on a file called user-details.sh to pull in the credentials. This file should contain similar to the following:
+#
+#USER="jonathan.dade@rightscale.com"
+#PASSWORD="MilRecHer6"
+#ACCT="7954"
+#
+
+# This file should setup variables containing the username and password
+source ./user-details.sh
+
+# This line sets up the login cookies
+curl -c mySavedCookies -u "$USER:$PASSWORD" https://my.rightscale.com/api/acct/$ACCT/login?api_version=1.0
+
+curl -b mySavedCookies -X POST -H 'X-API-VERSION:1.0' -d "right_script=412148" -d "server[parameters][PUBLIC_IP]=text:11.11.11.11" "https://my.rightscale.com/api/acct/$ACCT/servers/207486001/run_script"
+curl -b mySavedCookies -X POST -H 'X-API-VERSION:1.0' -d "right_script=https://my.rightscale.com/api/acct/$ACCT/right_scripts/412148" -d "server[parameters][PUBLIC_IP]=text:99.99.99.99" "https://my.rightscale.com/api/acct/$ACCT/servers/207486001/run_script"
