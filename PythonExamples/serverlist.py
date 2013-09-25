902c480c1a15cc0f5607ef55594dfe0aed89e952
Simon Vans-Colina <simon.vans-colina@pearson.com>
2013-09-25 16:57:56 +0100

    removed some un needed code and took a stab at using objectify

--- PythonExamples/serverlist.py
+++ PythonExamples/serverlist.py
@@ -5,7 +5,7 @@ import myrightscalepassword
 
 user="simon.vans-colina@pearson.com"
 acct="51401"
-baseurl="https://my.rightscale.com"
+baseurl="https://my.rightscale.com" # wont work because of the 302 that python turns into a GET 
 baseurl="https://us-3.rightscale.com"
 #password=getpass.getpass()
 password=myrightscalepassword.password
@@ -15,20 +15,15 @@ data = {}
 data['email'] = user
 data['password'] = password
 data['account_href'] = "/api/accounts/%s" % acct
+print 'data', data
 
-# reformat the string because rightscale doesnt understand form encoding (sadface)
-d="&".join(['%s=%s' % (k,data[k]) for k in data.keys()])
-
-print 'data', d
-
-headers = {'X-API-VERSION': "1.5", 'Content-Type': 'application/x-www-form-urlencoded'}
+headers = {'X-API-VERSION': "1.5", 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/xml'}
 print 'headers', headers
 
 url=baseurl+"/api/session"
-#url="http://localhost:9000/api/session"
 
 print url
-r=s.post(url, headers=headers, data=d)
+r=s.post(url, headers=headers, data=data)
 
 print 'r', r
 print 'raw', r.raw
@@ -36,5 +31,12 @@ print 'url', r.url
 print 'request', r.request
 print 'headers', r.headers
 
+r=s.get(baseurl+'/api/servers', headers=headers)
+print r.raw
+print r.text
 
+servers=objectify.fromstring(r.text)
+
+for server in servers:
+  print server
 
