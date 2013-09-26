1ffb634c213c42a81577d7b654963f433aa24aa7
Simon Vans-Colina <simon.vans-colina@pearson.com>
2013-09-26 09:46:26 +0100

    list servers and operational state

--- PythonExamples/serverlist.py
+++ PythonExamples/serverlist.py
@@ -7,7 +7,6 @@ user="simon.vans-colina@pearson.com"
 acct="51401"
 baseurl="https://my.rightscale.com" # wont work because of the 302 that python turns into a GET 
 baseurl="https://us-3.rightscale.com"
-#password=getpass.getpass()
 password=myrightscalepassword.password
 
 s = requests.Session()
@@ -15,28 +14,16 @@ data = {}
 data['email'] = user
 data['password'] = password
 data['account_href'] = "/api/accounts/%s" % acct
-print 'data', data
 
 headers = {'X-API-VERSION': "1.5", 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/xml'}
-print 'headers', headers
-
 url=baseurl+"/api/session"
-
-print url
 r=s.post(url, headers=headers, data=data)
-
-print 'r', r
-print 'raw', r.raw
-print 'url', r.url
-print 'request', r.request
-print 'headers', r.headers
-
 r=s.get(baseurl+'/api/servers', headers=headers)
-print r.raw
-print r.text
 
-servers=objectify.fromstring(r.text)
+servers=objectify.fromstring(str(r.text))
 
-for server in servers:
-  print server
+for server in servers.iterchildren():
+  print server.name, server.state
+
+
 
